9ae354a516f2c40fca02399b15a8e9048ddb1a0f
Tim Sarvey <timsarvey@gmail.com>
2016-03-09 09:56:24 -0500

    Add brookville band project info.

--- app/js/homeController.js
+++ app/js/homeController.js
@@ -11,13 +11,20 @@ angular.module('myApp')
 				img: './assets/img/starwars.jpg',
 				description: 'An Angular app developed using the <a href="http://swapi.co/" target="_blank">Star Wars API</a> to display basic information about Star Wars characters, films, starships, vehicles, species, planets, and their relations. If you are like me and you remember faces better than names, then this visual guide is right up your alley.',
 				gitHub: 'https://github.com/tbone849/star-wars-guide',
-				demo: 'https://starwars-visualguide.com/#/'
+				demo: 'https://starwars-visualguide.com'
 			},
 			{
 				title: 'Farmer Courses',
 				img: './assets/img/farmercourses.jpg',
 				description: 'A website for a company that provides Farm Service Agency approved courses that focus on improving profit and production while addressing finances and risk management. Users span from all over the United States, to some other countries as well. Developed in my pre-Angular days using JavaScript, jQuery, and jQueryUI.',
 				demo: 'http://farmercourses.com/'
+			},
+			{
+				title: 'Brookville Bands',
+				img: './assets/img/brkv.jpg',
+				description: 'A website I built for the Brookville Area Jr/Sr High School band program out of Brookville, PA. There are pages that showcase each band, a calendar of events, photos, etc. I built this web app using AngularJS and Lumx.',
+				gitHub: 'https://github.com/tbone849/brookville-bands',
+				demo: 'http://brookvillebands.com'
 			},
 			{
 				title: 'Weekly NFL Fantasy Leaders',
